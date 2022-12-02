25f9700c0346646364f97e6f18ce52b0ebb6e19f
sleeplessghost <sleeplessghost@live.com>
2022-12-03 00:12:46 +1030

    simplified formulas

--- 02.py
+++ 02.py
@@ -1,14 +1,14 @@
 def toInt(x):
     match x:
-        case 'A' | 'X': return 0
-        case 'B' | 'Y': return 1
-        case 'C' | 'Z': return 2
-        
+        case 'A' | 'X': return 1
+        case 'B' | 'Y': return 2
+        case 'C' | 'Z': return 3
+
 def part1Score(opp, me):
-    return 1 + me + (((me - opp + 1) % 3) * 3)
+    return 3 * ((me - opp + 1) % 3) + me
 
 def part2Score(opp, result):
-    return 1 + (result * 3) + ((opp + (result - 1)) % 3)
+    return 3 * (result - 1) + ((opp + result) % 3) + 1
 
 lines = [[toInt(x) for x in line.strip().split(' ')] for line in open('in/02.txt')]
 print('part1:', sum(part1Score(opp, me) for opp,me in lines))
