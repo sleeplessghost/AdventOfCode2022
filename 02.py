6703a53846a15654c7d02ae7e38829ecdd3f3bb4
sleeplessghost <sleeplessghost@live.com>
2022-12-02 16:42:50 +1030

    can do this as a formula

--- 02.py
+++ 02.py
@@ -3,14 +3,9 @@ def toInt(x):
         case 'A' | 'X': return 0
         case 'B' | 'Y': return 1
         case 'C' | 'Z': return 2
-
-def outcome(opp, me):
-    if me == opp: return 3
-    if me == (opp + 1) % 3: return 6
-    return 0
-
+        
 def part1Score(opp, me):
-    return 1 + me + outcome(opp, me)
+    return 1 + me + (((me - opp + 1) % 3) * 3)
 
 def part2Score(opp, result):
     return 1 + (result * 3) + ((opp + (result - 1)) % 3)
