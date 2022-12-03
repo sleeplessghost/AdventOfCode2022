e69e14863a6b654aef0c6b83466aeb1a4462d2dc
sleeplessghost <sleeplessghost@live.com>
2022-12-03 17:13:15 +1030

    cleaner + generator optimization

--- 03.py
+++ 03.py
@@ -6,8 +6,8 @@ def splitset(line):
     return set(line[:mid]), set(line[mid:])
 
 lines = [line.strip() for line in open('in/03.txt')]
-sets = [splitset(line) for line in lines]
-groups = [[set(line) for line in lines[i:i+3]] for i in range(0, len(lines), 3)]
+sets = map(splitset, lines)
+groups = (map(set, lines[i:i+3]) for i in range(0, len(lines), 3))
 
 print('part1:', sum(value(char) for a,b in sets for char in a.intersection(b)))
 print('part2:', sum(value(char) for a,b,c in groups for char in a.intersection(b).intersection(c)))
