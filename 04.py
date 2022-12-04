f8922b2c056ee6a04dccc616f4047b972459c4e1
sleeplessghost <sleeplessghost@live.com>
2022-12-04 16:21:40 +1030

    simplifying

--- 04.py
+++ 04.py
@@ -1,13 +1,12 @@
 
 def parsePart(part):
-    a,b = part.split('-')
-    return int(a),int(b)
+    return [int(n) for n in part.split('-')]
 
 def contains(a,b):
     return (min(a) <= min(b) and max(a) >= max(b)) or (min(b) <= min(a) and max(b) >= max(a))
 
 def overlaps(a,b):
-    return contains(a,b) or min(a) <= min(b) <= max(a) or min(a) <= max(b) <= max(a)
+    return min(a) <= min(b) <= max(a) or min(b) <= min(a) <= max(b)
 
 lines = [[parsePart(p) for p in line.strip().split(',')] for line in open('in/04.txt')]
 print('part1:', sum(contains(a,b) for a,b in lines))
