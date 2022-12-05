eabea2760fbd1a73cc81036566c29d493c6686ad
sleeplessghost <sleeplessghost@live.com>
2022-12-05 15:51:54 +1030

    ranges are already ordered

--- 04.py
+++ 04.py
@@ -2,12 +2,12 @@
 def parsePart(part):
     return tuple(int(n) for n in part.split('-'))
 
-def contains(a,b):
-    return (min(a) <= min(b) and max(a) >= max(b)) or (min(b) <= min(a) and max(b) >= max(a))
+def contains(a1,a2,b1,b2):
+    return (a1 <= b1 and a2 >= b2) or (b1 <= a1 and b2 >= a2)
 
-def overlaps(a,b):
-    return min(a) <= min(b) <= max(a) or min(b) <= min(a) <= max(b)
+def overlaps(a1,a2,b1,b2):
+    return a1 <= b1 <= a2 or b1 <= a1 <= b2
 
 lines = [[parsePart(p) for p in line.strip().split(',')] for line in open('in/04.txt')]
-print('part1:', sum(contains(a,b) for a,b in lines))
-print('part2:', sum(overlaps(a,b) for a,b in lines))
+print('part1:', sum(contains(*a,*b) for a,b in lines))
+print('part2:', sum(overlaps(*a,*b) for a,b in lines))
