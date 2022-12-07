f072d9d6fee8f93852e04652ecd9aa225ba49e6c
sleeplessghost <sleeplessghost@live.com>
2022-12-08 03:16:14 +1030

    File class isnt really necessary either

--- 07.py
+++ 07.py
@@ -8,17 +8,12 @@ class Folder:
 
     def size(self):
         if self.cachedSize == None:
-            self.cachedSize = sum(d.size() for d in self.folders) + sum(f.size for f in self.files)
+            self.cachedSize = sum(f.size() for f in self.folders) + sum(size for size,name in self.files)
         return self.cachedSize
 
     def search(self, searchFunction):
         if searchFunction(self): yield self
         yield from (match for f in self.folders for match in f.search(searchFunction))
-
-class File:
-    def __init__(self, size, name):
-        self.size = int(size)
-        self.name = name
 
 def parseLines(root, lines):
     current: Folder = root
@@ -26,15 +21,13 @@ def parseLines(root, lines):
         match line.split():
             case '$', 'cd', '/': current = root
             case '$', 'cd', '..': current = current.parent
-            case '$', 'cd', name: current = next(d for d in current.folders if d.name == name)
+            case '$', 'cd', name: current = next(f for f in current.folders if f.name == name)
             case '$', 'ls': continue
             case 'dir', name: current.folders.append(Folder(current, name))
-            case size, name: current.files.append(File(size, name))
+            case size, name: current.files.append((int(size), name))
 
 root = Folder(None, 'root')
-lines = [line.strip() for line in open('in/07.txt')]
-parseLines(root, lines)
-space_to_clear = 30_000_000 - 70_000_000 + root.size()
-
+parseLines(root, open('in/07.txt').read().splitlines())
+space_to_clear = 30_000_000 - (70_000_000 - root.size())
 print('part1:', sum(d.size() for d in root.search(lambda dir: dir.size() <= 100_000)))
 print('part2:', min(d.size() for d in root.search(lambda dir: dir.size() >= space_to_clear)))
