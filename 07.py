32dc631b292597584211cad2f98093d89c180a85
sleeplessghost <sleeplessghost@live.com>
2022-12-07 22:43:05 +1030

    tidying

--- 07.py
+++ 07.py
@@ -1,21 +1,46 @@
 
-class Dir:
+class Folder:
     def __init__(self, parent, name):
         self.parent = parent
         self.name = name
-        self.subdirs = []
+        self.folders = []
         self.files = []
+        self.cachedSize = None
+
+    def size(self):
+        if self.cachedSize == None:
+            self.cachedSize = sum(d.size() for d in self.folders) + sum(f.size for f in self.files)
+        return self.cachedSize
+
+    def search(self, searchFunction):
+        searchResults = []
+        if searchFunction(self): searchResults.append(self)
+        for folder in self.folders:
+            matches = folder.search(searchFunction)
+            searchResults.extend(matches)
+        return searchResults
 
 class File:
     def __init__(self, size, name):
         self.size = size
         self.name = name
 
+def parseLines(root, lines):
+    current = root
+    for line in lines:
+        match line.split():
+            case '$', 'cd', '/': current = root
+            case '$', 'cd', '..': current = current.parent
+            case '$', 'cd', name: current = mkdir(current, name)
+            case '$', 'ls': continue
+            case 'dir', name: mkdir(current, name)
+            case size, name: mkfile(current, size, name)
+
 def mkdir(dir, name):
-    sub = next((d for d in dir.subdirs if d.name == name), None)
+    sub = next((d for d in dir.folders if d.name == name), None)
     if sub == None:
-        sub = Dir(dir, name)
-        dir.subdirs.append(sub)
+        sub = Folder(dir, name)
+        dir.folders.append(sub)
     return sub
 
 def mkfile(dir, size, name):
@@ -25,53 +50,10 @@ def mkfile(dir, size, name):
         dir.files.append(file)
     return file
 
-def lsdir(dir, lines, i):
-    x = i+1
-    part = lines[x].split(' ')
-    while x < len(lines) and (part := lines[x].split(' '))[0] != '$':
-        if part[0] == 'dir': mkdir(dir, part[1])
-        else: mkfile(dir, part[0], part[1])
-        x += 1
-    return x
-
-def dirsize(dir):
-    return sum(f.size for f in dir.files) + sum(dirsize(d) for d in dir.subdirs)
-
-def checkdir(dir, list, size):
-    #flip for part1
-    if dirsize(dir) >= size:
-        list.append(dir)
-    for d in dir.subdirs:
-        checkdir(d, list, size)
-
+root = Folder(None, 'root')
 lines = [line.strip() for line in open('in/07.txt')]
+parseLines(root, lines)
+space_to_clear = 30_000_000 - 70_000_000 + root.size()
 
-root = Dir(None, 'root')
-current = root
-i = 0
-while i < len(lines):
-    parts = lines[i].split(' ')
-    if parts[0] == '$':
-        if parts[1] == 'cd':
-            if parts[2] == '/': current = root
-            elif parts[2] == '..': current = current.parent
-            else: current = mkdir(current, parts[2])
-            i += 1
-        elif parts[1] == 'ls':
-            i = lsdir(current, lines, i)
-            
-resu = []
-checkdir(root, resu, 100000)
-
-print('part1:', sum(dirsize(d) for d in resu))
-
-rtsize = dirsize(root)
-freespace = 70000000 - rtsize
-requiredremove = 30000000 - freespace
-
-resu2 = []
-checkdir(root, resu2, requiredremove)
-totals = [dirsize(d) for d in resu2]
-
-
-print('part2:', min(totals))
+print('part1:', sum(d.size() for d in root.search(lambda dir: dir.size() <= 100_000)))
+print('part2:', min(d.size() for d in root.search(lambda dir: dir.size() >= space_to_clear)))
