86f8ea072b55ca61673137a71280c8ac92605ed9
sleeplessghost <sleeplessghost@live.com>
2022-12-09 23:43:35 +1030

    more tidying

--- 09.py
+++ 09.py
@@ -1,33 +1,22 @@
-from collections import defaultdict
-
-def parse(line):
-    char, num = line.split(' ')
-    return [char, int(num)]
-
 def calcTail(hx, hy, tx, ty):
-    if abs(hx-tx) + abs(hy-ty) > 2:
-        tx += 1 if hx >= tx else -1
-        ty += 1 if hy >= ty else -1
-    else:
-        if abs(hx-tx) >= 2: tx += 1 if hx > tx else -1
-        if abs(hy-ty) >= 2: ty += 1 if hy > ty else -1
+    combined = (dist_x := abs(hx-tx)) + (dist_y := abs(hy-ty))
+    if dist_x >= 2 or combined > 2: tx += 1 if hx > tx else -1
+    if dist_y >= 2 or combined > 2: ty += 1 if hy > ty else -1
     return (tx,ty)
 
-def solve(steps, numKnots):
-    knots = [(0,0) for _ in range(numKnots + 1)]
+def solve(steps, knots):
+    rope = [(0,0) for _ in range(knots + 1)]
     visited = set()
-    directions = { 'R':(1,0), 'L':(-1,0), 'U':(0,-1), 'D':(0,1)}
+    directions = {'R':(1,0), 'L':(-1,0), 'U':(0,-1), 'D':(0,1)}
     for char, num in steps:
-        for _ in range(num):
-            (hx,hy), (dx,dy) = knots[0], directions[char]
-            knots[0] = (hx + dx, hy + dy)
-
-            for i in range(1, len(knots)):
-                knots[i] = calcTail(*knots[i-1], *knots[i])
-            visited.add(knots[len(knots)-1])
+        for _ in range(int(num)):
+            (hx,hy), (dx,dy) = rope[0], directions[char]
+            rope[0] = (hx + dx, hy + dy)
+            for i in range(1, len(rope)):
+                rope[i] = calcTail(*rope[i-1], *rope[i])
+            visited.add(rope[-1])
     return len(visited)
 
-input = open('in/09.txt').read()
-steps = [parse(l) for l in input.splitlines()]
+steps = [line.strip().split() for line in open('in/09.txt')]
 print('part1:', solve(steps, 1))
 print('part2:', solve(steps, 9))
