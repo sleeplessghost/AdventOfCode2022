0a72042cc39300e1b45d86341154b0ba15b64f57
sleeplessghost <sleeplessghost@live.com>
2022-12-09 23:30:12 +1030

    tidying

--- 09.py
+++ 09.py
@@ -4,31 +4,27 @@ def parse(line):
     char, num = line.split(' ')
     return [char, int(num)]
 
+def calcTail(hx, hy, tx, ty):
+    if abs(hx-tx) + abs(hy-ty) > 2:
+        tx += 1 if hx >= tx else -1
+        ty += 1 if hy >= ty else -1
+    else:
+        if abs(hx-tx) >= 2: tx += 1 if hx > tx else -1
+        if abs(hy-ty) >= 2: ty += 1 if hy > ty else -1
+    return (tx,ty)
+
 def solve(steps, numKnots):
     knots = [(0,0) for _ in range(numKnots + 1)]
     visited = set()
+    directions = { 'R':(1,0), 'L':(-1,0), 'U':(0,-1), 'D':(0,1)}
     for char, num in steps:
         for _ in range(num):
-            hx,hy = knots[0]
-            match char:
-                case 'R': hx += 1
-                case 'L': hx -= 1
-                case 'U': hy -= 1
-                case 'D': hy += 1
-            knots[0] = (hx,hy)
+            (hx,hy), (dx,dy) = knots[0], directions[char]
+            knots[0] = (hx + dx, hy + dy)
 
             for i in range(1, len(knots)):
-                hx,hy = knots[i-1]
-                tx,ty = knots[i]
-                if hy == ty:
-                    if abs(hx - tx) >= 2: tx += 1 if hx > tx else -1
-                elif hx == tx:
-                    if abs(hy - ty) >= 2: ty += 1 if hy > ty else -1
-                elif abs(hx-tx) + abs(hy-ty) > 2:
-                    tx += 1 if hx >= tx else -1
-                    ty += 1 if hy >= ty else -1
-                knots[i] = (tx,ty)
-                if i == len(knots)-1: visited.add((tx,ty))
+                knots[i] = calcTail(*knots[i-1], *knots[i])
+            visited.add(knots[len(knots)-1])
     return len(visited)
 
 input = open('in/09.txt').read()
