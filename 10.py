dc3ee586863a4b29bbda9c87aca2c46d6e793abc
sleeplessghost <sleeplessghost@live.com>
2022-12-10 16:03:20 +1030

    tidy a bit

--- 10.py
+++ 10.py
@@ -1,40 +1,28 @@
-
-
 from collections import defaultdict
 
-
-input = open('in/10.txt').read()
-split = input.splitlines()
-
-x = 1
-i = 0
-cycle = 0
-strengths = []
-instr = []
-outs = defaultdict(bool)
+instr = open('in/10.txt').read().splitlines()
+register, cycle, i, signal = 1, 0, 0, 0
+pending_adds, pixels = [], defaultdict(bool)
 
 while True:
-    pos_x = cycle % 40
-    pos_y = cycle // 40
+    x = cycle % 40
+    y = cycle // 40
     cycle += 1
-    if pos_x in (x-1, x, x+1):
-        outs[(pos_x, pos_y)] = True
-    if cycle in [20, 60, 100, 140, 180, 220]:
-        strengths.append(x * cycle)
-    if len(instr):
-        x += instr.pop()
-    elif (i >= len(split)): break
+    if x in (register-1, register, register+1):
+        pixels[(x, y)] = True
+    if cycle in (20, 60, 100, 140, 180, 220):
+        signal += register * cycle
+    if len(pending_adds):
+        register += pending_adds.pop()
+    elif (i >= len(instr)): break
     else:
-        match split[i].split():
-            case 'addx', num:
-                instr.append(int(num))
+        match instr[i].split():
+            case 'addx', num: pending_adds.append(int(num))
         i += 1
 
-print('part1:', sum(strengths))
-
-for y in range(max(y for (x,y) in outs.keys())+1):
-    for x in range(max(x for (x,y) in outs.keys())+1):
-        print('#' if outs[(x,y)] else '.', end='')
+print('part1:', signal)
+print('part2:')
+for y in range(max(y for (x,y) in pixels.keys())+1):
+    for register in range(max(x for (x,y) in pixels.keys())+1):
+        print('██' if pixels[(register,y)] else '  ', end='')
     print()
-
-print('part2:', 0)
